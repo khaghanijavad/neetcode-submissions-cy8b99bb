b507ea62795d3ada9282eb26c0ca54818ab3f865
Javad Khaghani <khaghani.javad@gmail.com>
2026-05-03 20:51:53 -0700

    Add: search-for-word - submission-8

---
+++ Data Structures & Algorithms/search-for-word/submission-8.py
@@ -0,0 +1,26 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        visit = set()
+
+        def dfs(r, c, i):
+            if (not ((0 <= r < rows) and (0 <= c < cols))) or ((r, c) in visit) or (board[r][c] != word[i]):
+                return False
+            
+            # Ordering of two condtions matter, to move this up before (board[r][c] != word[i]), ((r, c) in visit), (not ((0 <= r < rows) and (0 <= c < cols))), should change it to i==len(word), 
+            # Otherwise we are telling that if it is the last char, don't care whether board/word match or not!
+            if i == len(word) - 1:
+                return True
+
+
+            visit.add((r, c))
+            out = dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1)
+            visit.remove((r, c))
+
+            return out
+
+        for i in range(rows):
+            for j in range(cols):
+                if dfs(i, j, 0):
+                    return True
+        return False
